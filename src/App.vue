46819758b5c08dcd704af6fedbff8652cdbd83a7
Fredrick W. Warren <fredw@northriverboats.com>
2019-10-15 15:11:39 -0700

    Added Loading Overlay

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
+    <div class="vld-parent">
+      <loading :active.sync="loaded"
+        :can-cancel="false"
+        :is-full-page="true">
+      </loading>
+    </div>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'App',
+  components: {
+    Loading
+  },
   computed: {
     ...mapGetters([
       'isLoading'
-    ])
+    ]),
+    loaded: function () {
+      return this.isLoading > 0
+    }
   }
 }
 </script>
