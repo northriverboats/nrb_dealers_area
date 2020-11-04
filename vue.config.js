0b6cdbf8335aec7a529f8b776a108b77ff5b5124
Fredrick W. Warren <fredw@northriverboats.com>
2020-11-04 22:24:20 +0000

    additional changes for hotlaoding to work

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 // vue.config.js
+const fs = require("fs");
+
 module.exports = {
   publicPath: '/dealers-area/',
   devServer: {
-    https: true,
-    disableHostCheck: true
+    host: '0.0.0.0',
+    headers: {
+      'Access-Control-Allow-Origin': 'https://10.10.200.88:8080',
+    },
+    disableHostCheck: true,
+    public: 'www.northriverboats.com:8080',
+    https: {
+      key: fs.readFileSync("./certs/localhost-key.pem"),
+      cert: fs.readFileSync("./certs/localhost.pem")
+    },
   }
 }
