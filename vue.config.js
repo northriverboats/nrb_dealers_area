8f080dd4e99438fa820be90a85672c4604883c94
Fredrick W Warren <fredwarren@gmail.com>
2021-05-06 17:05:08 +0000

    dev allow cross origin

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,12 @@ module.exports = {
   publicPath: '/dealers-area/',
   devServer: {
     host: '0.0.0.0',
+    port: 8090,
     headers: {
-      'Access-Control-Allow-Origin': 'https://10.10.200.88:8080',
+      'Access-Control-Allow-Origin': 'https://10.10.200.225:8090',
     },
     disableHostCheck: true,
-    public: 'www.northriverboats.com:8080',
+    public: 'www.northriverboats.com:8090',
     https: {
       key: fs.readFileSync("./certs/localhost-key.pem"),
       cert: fs.readFileSync("./certs/localhost.pem")
