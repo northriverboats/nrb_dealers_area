e5b80e8d99265b10db6283f5dbe8bb7832a63798
Fredrick W Warren <fredwarren@gmail.com>
2022-11-16 16:13:48 -0800

    remove ssl from vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,14 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     port: 8090,
-    headers: {
-      'Access-Control-Allow-Origin': 'https://10.10.200.225:8090',
-    },
+    // headers: {
+    //  'Access-Control-Allow-Origin': 'https://10.10.200.225:8090',
+    // },
     disableHostCheck: true,
-    public: 'www.northriverboats.com:8090',
-    https: {
-      key: fs.readFileSync("./certs/localhost-key.pem"),
-      cert: fs.readFileSync("./certs/localhost.pem")
-    },
+    // public: 'www.northriverboats.com:8090',
+    // https: {
+    //   key: fs.readFileSync("./certs/localhost-key.pem"),
+    //  cert: fs.readFileSync("./certs/localhost.pem")
+    // },
   }
 }
