0f0535c5208f5252e4b3aee06d944ebc132d38ae
Fredrick W Warren <fredwarren@gmail.com>
2022-11-16 16:16:14 -0800

    adding files for dev/prod password auth

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <?php
-/*** PREVENT THE PAGE FROM BEING CACHED BY THE WEB BROWSER ***/header("Cache-Control: no-cache, must-revalidate");
+/*** PREVENT THE PAGE FROM BEING CACHED BY THE WEB BROWSER ***/
+header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
 require_once("wp-authenticate.php");
 
-/*** REQUIRE USER AUTHENTICATION ***/login();
+/*** REQUIRE USER AUTHENTICATION ***/
+login();
 
-/*** RETRIEVE LOGGED IN USER INFORMATION ***/$user = wp_get_current_user();
+/*** RETRIEVE LOGGED IN USER INFORMATION ***/
+$user = wp_get_current_user();
 
 // $page = file_get_contents('http://10.10.200.245:8080');
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://10.10.200.245:8080');
+curl_setopt($ch, CURLOPT_URL, 'http://nrb-dealers-area:8090/dealers-area/');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $page = curl_exec($ch);
 curl_close($ch);
 
 $html = (explode("\n",$page));
 $size = count($html);
-$head = array_search("  </head>", $html);
+// $head = array_search("</head>", $html);
+$head = 9;
 
 ?>
 <!DOCTYPE html>
@@ -28,13 +32,13 @@ for($i = 3; $i < $head; $i++) {
     echo $html[$i] . "\n";
 }
 ?>
-</head>
+<!-- </head> -->
 <body>
 <script>
 window.nonce = "<?php echo wp_create_nonce( 'wp_rest' ); ?>"
 </script>
 <?php
-for($i = $head + 2; $i < $size -2; $i++) {
+for($i = $head + 1; $i < $size -2; $i++) {
     echo $html[$i] . "\n";
 }
 /*** Trunk for junk
